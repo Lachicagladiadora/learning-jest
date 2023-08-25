f0fa17cdd5f8f090057616f356fa75afa4d2c323
Lachicagladiadora <200379@unsaac.edu.pe>
2023-08-24 22:18:44 -0500

    negation value

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,9 @@
 const sum = require('./sum')
 
-test('adds 1 + 2 to equal 3', () => {
+test('first test: adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3)
+})
+
+test('sum is different of zero ', ()=>{
+  expect(sum(4,9)).not.toBe(0)
 })
